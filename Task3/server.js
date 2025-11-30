c5ab4ea4b9966749502f517f0f88ec477795821a
tanyajha29 <jhatanya211@gmail.com>
2025-12-01 00:06:29 +0530

    done with task3 will add some more featues later on

--- Task3/server.js
+++ Task3/server.js
@@ -1,19 +1,48 @@
-import express from "express";
-import path from "path";
+// ---------------- CommonJS Version -----------------
+const express = require("express");
+const path = require("path");
+
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(process.cwd(), "Task3/public")));
+app.use(express.json());
 
+// Set EJS
 app.set("view engine", "ejs");
-app.set("views", path.join(process.cwd(), "Task3/views"));
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => res.render("index"));
-app.get("/about", (req, res) => res.render("about"));
-app.get("/contact", (req, res) => res.render("contact"));
+// Public folder
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/contact-submit", (req, res) => {
-    res.render("contact", { message: "Message sent successfully!" });
+// Routes
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.listen(3000, () => console.log("Task 3 running at http://localhost:3000"));
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.post("/submit", (req, res) => {
+    const { fullname, username, email, password } = req.body;
+
+    res.render("result", {
+        fullname,
+        username,
+        email,
+        password
+    });
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
